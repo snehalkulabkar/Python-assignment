dd18f77f686154b3933d92203cb967912513aa95
snehalkulabkar <68235000+snehalkulabkar@users.noreply.github.com>
2020-07-22 09:19:52 +0530

    Day 2 assignment uploading

---
+++ day 2/Untitled.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print ("Hello")
+
+
+# In[6]:
+
+
+print ("Hello         Python")
+
+
+# In[7]:
+
+
+print ("""Hello World""")
+
+
+# In[8]:
+
+
+print ("I'm Snehal")
+
+
+# In[9]:
+
+
+print ('Hello everyone')
+
+
+# In[11]:
+
+
+print ("Hello\tPython")
+
+
+# In[12]:
+
+
+print ("hello\nPython")
+
+
+# 
+
+# In[22]:
+
+
+name = "snehal"
+marks = 90.99
+age = 19
+print ("the name of person is", name, "marks is", marks,"age is",age)
+
+
+# In[23]:
+
+
+print ("name of person is %s marks is %f age is %d"%(name,marks, age))
+
+
+# In[24]:
+
+
+10+5
+
+
+# In[25]:
+
+
+10-5
+
+
+# In[27]:
+
+
+13*7
+
+
+# In[28]:
+
+
+15/5
+
+
+# In[29]:
+
+
+10%3
+
+
+# In[30]:
+
+
+10//3
+
+
+# In[31]:
+
+
+a=10
+b=20
+a == b
+
+
+# In[32]:
+
+
+c=10
+a == c
+
+
+# In[33]:
+
+
+c <= a
+
+
+# In[34]:
+
+
+a >= b
+
+
+# In[36]:
+
+
+a != b
+
+
+# In[38]:
+
+
+a is b
+
+
+# In[39]:
+
+
+a = 10
+b = 10
+a == b
+
+
+# In[40]:
+
+
+a is b
+
+
+# In[41]:
+
+
+id(a)
+
+
+# In[43]:
+
+
+id(b)
+
+
+# In[45]:
+
+
+a = 257
+b =257
+a ==b
+
+
+# In[46]:
+
+
+a is b
+
+
+# In[47]:
+
+
+a > 20
+
+
+# In[48]:
+
+
+a = 10
+b =20
+a==b
+
+
+# In[49]:
+
+
+a > 20 or 10 >9
+
+
+# In[51]:
+
+
+a > 20 and 10 >9
+
+
+# In[52]:
+
+
+10 < 9 and M
+
+
+# In[ ]:
+
+
+
+
+
+# 
